17d0c62ba096faf7841a7a7f89016f6f77756a87
loczek <30776250+loczek@users.noreply.github.com>
2022-08-31 01:31:36 +0200

    remove old files

--- src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-export const t = (s: TemplateStringsArray) =>
-  process.env.LANG === "ts" ? ": " + s.raw[0] : null;
-
-export const tm = (s: TemplateStringsArray) =>
-  process.env.LANG === "ts" ? s.raw[0] : null;
-
-export const ti = (s: TemplateStringsArray) =>
-  process.env.LANG === "ts" ? ", " + s.raw[0] : null;
