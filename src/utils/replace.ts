f070efd802e18cc2fb77375c005047f250f06791
loczek <30776250+loczek@users.noreply.github.com>
2022-08-31 22:28:33 +0200

    fix: add missing variables

---
+++ src/utils/replace.ts
@@ -0,0 +1,11 @@
+export const fileNameCapitalizedReplace = "${1:${TM_FILENAME_BASE/(.)/${1:/upcase}/}}";
+export const fileNameReplace = "";
+export const firstReplace = "${1}";
+export const secondReplace = "${2}";
+export const thirdReplace = "${3}";
+
+export const fileNameCapitalizedReplaceMd = "FileName";
+export const fileNameReplaceMd = "filename";
+export const firstReplaceMd = "";
+export const secondReplaceMd = "";
+export const thirdReplaceMd = "";
