5eafbe89f2564753cb3f40cef465fa8343a4e5ee
loczek <30776250+loczek@users.noreply.github.com>
2022-08-31 22:26:53 +0200

    change na to napi and nm to nmid

--- src/snippets/lang.ts
+++ src/snippets/lang.ts
@@ -1,27 +1,28 @@
 import { Snippet } from "../gen";
+import { first } from "../utils/placeholders";
 import { tsImport, tsSpace, tsType } from "../utils/typescript";
 
 export const generateNextLang = () => {
   const NextApi: Snippet = {
-    prefix: "na",
+    prefix: "napi",
     body: [
       tsImport("import type { NextApiRequest, NextApiResponse } from 'next'"),
       tsSpace(),
       tsImport("interface Data {}"),
       tsSpace(),
       `export default (req${tsType("NextApiRequest")}, res${tsType("NextApiResponse<Data>")}) => {`,
-      "  ${1}",
+      `  ${first}`,
       "}",
     ],
   };
   const NextMiddleware: Snippet = {
-    prefix: "nm",
+    prefix: "nmid",
     body: [
       "import { NextResponse } from 'next/server'",
       tsImport("import type { NextRequest } from 'next/server'"),
       tsSpace(),
       `export async function middleware(request${tsType("NextRequest")}) {`,
-      "  ${1}",
+      `  ${first}`,
       "}",
       "",
       "export const config = {",
@@ -32,31 +33,3 @@ export const generateNextLang = () => {
 
   return [NextApi, NextMiddleware];
 };
-
-// "nextApi": {
-//   "prefix": ["na"],
-//   "body": [
-//     "import type { NextApiRequest, NextApiResponse } from 'next'",
-//     "",
-//     "interface Data {}",
-//     "",
-//     "export default (req: NextApiRequest, res: NextApiResponse<Data>) => {",
-//     "  {$1}",
-//     "}"
-//   ]
-// },
-// "nextMiddleware": {
-//   "prefix": ["nm"],
-//   "body": [
-//     "import { NextResponse } from 'next/server'",
-//     "import type { NextRequest } from 'next/server'",
-//     "",
-// "export async function middleware(request: NextRequest) {",
-// "  ${1}",
-// "}",
-// "",
-// "export const config = {",
-// "  matcher: '/about/:path*',",
-// "}"
-//   ]
-// }
